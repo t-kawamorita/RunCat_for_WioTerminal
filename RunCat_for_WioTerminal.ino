9aa534bd58630b0ef13cb8be599f503f2c5f9808
t.kawamorita <t.kawamorita@oec-ltd.co.jp>
2020-08-28 12:15:51 +0900

    speedup

--- RunCat_for_WioTerminal.ino
+++ RunCat_for_WioTerminal.ino
@@ -6,116 +6,127 @@
 #include "cat_320x240.h"
 
 static LGFX lcd;
-static LGFX_Sprite canvas(&lcd);
-static LGFX_Sprite cat0(&canvas);
 
 static auto transpalette = 0;
 
-#define DEF_CAT_FILE(str) { cat0_##str##_width, \
-                            cat0_##str##_height, \
-                            cat0_##str##_d, \
-                            cat1_##str##_d, \
-                            cat2_##str##_d, \
-                            cat3_##str##_d, \
-                            cat4_##str##_d, \
-                            cat_sleep_##str##_d }
+#define DEF_CAT_FILE(str)    \
+  {                          \
+    cat0_##str##_width,      \
+        cat0_##str##_height, \
+        cat0_##str##_d,      \
+        cat1_##str##_d,      \
+        cat2_##str##_d,      \
+        cat3_##str##_d,      \
+        cat4_##str##_d,      \
+        cat_sleep_##str##_d  \
+  }
 
-typedef struct {
+typedef struct
+{
   const int width;
   const int height;
-  const unsigned char* cat[6];
+  const unsigned char *cat[6];
 } CAT_FILE;
 
 CAT_FILE cats[4] = {
-  DEF_CAT_FILE(32x24) ,
-  DEF_CAT_FILE(80x60) ,
-  DEF_CAT_FILE(160x120) ,
-  DEF_CAT_FILE(320x240) ,
+    DEF_CAT_FILE(32x24),
+    DEF_CAT_FILE(80x60),
+    DEF_CAT_FILE(160x120),
+    DEF_CAT_FILE(320x240),
 };
 
-int cursor = 0;
+#define CAT_WIDTH (cats[catSize].width)
+#define CAT_HEIGHT (cats[catSize].height - 1)
+#define CAT_IMAGE cats[catSize].cat
 
-#define CAT_WIDTH (cats[cursor].width)
-#define CAT_HEIGHT (cats[cursor].height)
-#define CAT_IMAGE cats[cursor].cat
+#define DELAY_TIME 50
 
-void setup() {
+void setup()
+{
   pinMode(WIO_5S_LEFT, INPUT_PULLUP);
   pinMode(WIO_5S_RIGHT, INPUT_PULLUP);
+  pinMode(WIO_5S_UP, INPUT_PULLUP);
+  pinMode(WIO_5S_DOWN, INPUT_PULLUP);
 
   lcd.init();
   lcd.setRotation(1);
   lcd.setFont(&fonts::FreeMono9pt7b);
-  lcd.fillScreen(lcd.color888(0,0,0));
-
-  canvas.setColorDepth(lgfx::palette_4bit);
-  canvas.createSprite(lcd.width(), lcd.height());
-  canvas.fillScreen(transpalette);
-  cat0.setColorDepth(lgfx::palette_4bit);  
-  cat0.createSprite(CAT_WIDTH, CAT_HEIGHT);
+  lcd.fillScreen(lcd.color888(0, 0, 0));
 }
 
+#define INTERVAL_TIME 10000
 
-
-void drawCat(unsigned int n)
+void loop()
 {
-  unsigned char* p;
-  if ( n <= 4 ) {
-    p = (unsigned char*)CAT_IMAGE[n];
-  } else {
-    p = (unsigned char*)CAT_IMAGE[5];
-  }
+  unsigned long currTime;
+  unsigned long drawTime;
+  unsigned long stateTime;
+  int catSize = 0;
+  bool run = true;
+  int catForm = 0;
+  int waitTime = DELAY_TIME;
+  int key = 0;
 
-  for (int y=0; y<CAT_HEIGHT; y++) {
-    for (int x=0; x<CAT_WIDTH; ) {
-      unsigned char bits = *p++;
-      for ( int b=0; b<8; b++) {
-        if ( bits & 0x80 ) {
-          cat0.drawPixel( x, y, 15 );
-        } else {
-          cat0.drawPixel( x, y, transpalette);
-        }
-        bits <<=1;
-        x++;
+  currTime = drawTime = stateTime = millis();
+
+  while (true)
+  {
+    int catSizeOld = catSize;
+
+    while (true)
+    {
+      currTime = millis();
+      if (drawTime > currTime || (currTime - drawTime) > waitTime)
+      {
+        break;
+      }
+      delay(1);
+
+      if (stateTime > currTime || (currTime - stateTime) > INTERVAL_TIME)
+      {
+        run = !run;
+        stateTime = currTime;
+      }
+
+      key = catSize * 10000 + waitTime;
+      if (digitalRead(WIO_5S_LEFT) == LOW)
+        catSize--;
+      if (digitalRead(WIO_5S_RIGHT) == LOW)
+        catSize++;
+      if (digitalRead(WIO_5S_DOWN) == LOW)
+        waitTime--;
+      if (digitalRead(WIO_5S_UP) == LOW)
+        waitTime++;
+
+      catSize = min(3, max(0, catSize));
+      waitTime = min(1000, max(0, waitTime));
+
+      if (key != (catSize * 10000 + waitTime))
+      {
+        delay(100);
       }
     }
-  }
-}
 
-void loop() {
-  int run = 0;
-  int limit = 100;
-  int c = 0; 
-  
-  while ( true ) {
-    int ocursor = cursor;
-    if ( digitalRead(WIO_5S_LEFT) == LOW ) {
-      cursor = cursor > 0 ? cursor-1 : cursor;
-    }
-    if ( digitalRead(WIO_5S_RIGHT) == LOW ) {
-      cursor = cursor < 3 ? cursor+1 : cursor;
-    }
-    if ( ocursor != cursor ) {
-      cat0.deleteSprite();
-      cat0.createSprite(CAT_WIDTH, CAT_HEIGHT);
-      canvas.fillScreen(transpalette);
+    if (catSizeOld != catSize)
+    {
+      lcd.fillScreen(0);
       delay(300);
     }
 
     int ofsX = (lcd.width() - CAT_WIDTH) / 2;
     int ofsY = (lcd.height() - CAT_HEIGHT) / 2;
 
-    drawCat( run ? c : 5 );
     lcd.startWrite();
-    canvas.pushSprite(0,0);
-    cat0.pushSprite(ofsX,ofsY);
+    lcd.drawBitmap(ofsX,
+                   ofsY,
+                   CAT_IMAGE[run ? catForm : 5],
+                   CAT_WIDTH,
+                   CAT_HEIGHT,
+                   TFT_WHITE,
+                   TFT_BLACK);
     lcd.endWrite();
+    drawTime = millis();
 
-    if ( limit == 0 ) {
-      run = 1 - run;
-      limit = 100;
-    }
-    c = (c+1) % 5;
-    limit--;
+    catForm = (catForm + 1) % 5;
   }
 }
